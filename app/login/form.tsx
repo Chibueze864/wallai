662f1406b8dd2277e9c8bd3e8cdad935e233b0f9
Chibueze864 <66389146+Chibueze864@users.noreply.github.com>
2024-07-13 15:59:01 +0100

    Update form.tsx

--- app/login/form.tsx
+++ app/login/form.tsx
@@ -78,7 +78,7 @@ const LoginForm = () => {
         </button>
       </form>
       <div className="text-sm flex justify-between items-center mt-3">
-        <p>If you don't have an account...</p>
+        <p>If you do not have an account...</p>
         <Link href="/register" className="py-2 px-5 ml-3 bg-white border rounded-xl hover:scale-110 duration-300 border-blue-400">
           Register
         </Link>
